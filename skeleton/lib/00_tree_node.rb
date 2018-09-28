53ad3fd1c91c9563ba767403e87ed45c71889f5e
App Academy Student <ssoonmi@gmail.com>
2018-09-28 13:53:30 -0400

    Finish PolyTreeNode class with search algorithms dfs and bfs

--- skeleton/lib/00_tree_node.rb
+++ skeleton/lib/00_tree_node.rb
@@ -1,6 +1,7 @@
+require 'byebug'
+
 class PolyTreeNode
-  attr_reader :value, :parent
-  attr_accessor :children
+  attr_reader :value, :parent, :children
 
   def initialize(value, parent=nil)
     @value = value
@@ -8,7 +9,49 @@ class PolyTreeNode
     @children = []
   end
 
-  def parent=
-
+  def parent=(new_parent)
+    old_parent = self.parent
+    old_parent.children.delete(self) unless old_parent == nil
+    @parent = new_parent
+    unless new_parent == nil || new_parent.children.include?(self)
+      new_parent.children << self
+    end
   end
+
+  def add_child(child_node)
+    child_node.parent = self
+  end
+
+  def remove_child(child_node)
+    raise "Node is not a child" if child_node.parent != self
+    child_node.parent = nil
+  end
+
+  def dfs(target_value)
+    return self if self.value == target_value
+
+    self.children.each do |child|
+      target_node = child.dfs(target_value)
+      return target_node unless target_node.nil?
+    end
+
+    nil
+  end
+
+  def bfs(target_value)
+    nodes_queue = []
+    nodes_queue << self
+
+    until nodes_queue.empty?
+      current_node = nodes_queue.shift
+      return current_node if current_node.value == target_value
+      nodes_queue += current_node.children
+    end
+    
+    nil
+  end
+
+  private
+
+  attr_writer :children
 end
